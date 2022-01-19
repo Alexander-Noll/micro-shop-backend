d06bf6004537712d33806e42c31db6e532a84ec0
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2022-01-19 19:26:12 +0100

    added setPrice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SetPriceDto } from './common/SetPriceDto';
 import { BuildEvent } from './modules/builder/build-event.schema';
 
 @Controller()
@@ -59,6 +60,16 @@ export class AppController implements OnModuleInit {
   async postEvent(@Body() event: BuildEvent) {
     try {
       return await this.appService.handleEvent(event);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Post('cmd/setPrice')
+  async postCommand(@Body() params: SetPriceDto) {
+    try {
+      const c = await this.appService.setPrice(params);
+      return c
     } catch (error) {
       return error;
     }
